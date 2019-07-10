e841a878accd71d4a8e746ac5ae48ad86faabdd5
paoramati <brandonmitchellpaul@gmail.com>
2019-07-10 23:27:26 +1200

    protecting resources

--- src/main/java/com/bmp/myapi/TestEndpoint.java
+++ src/main/java/com/bmp/myapi/TestEndpoint.java
@@ -1,5 +1,7 @@
 package com.bmp.myapi;
 
+import com.bmp.myapi.auth.Authorize;
+import com.bmp.myapi.model.Data.Resource;
 import com.google.gson.Gson;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -10,6 +12,7 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 /**
  * Root resource (exposed at "test" path)
@@ -18,8 +21,8 @@ import javax.ws.rs.core.Response;
 public class TestEndpoint {
 
     /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
+     * Method handling HTTP GET requests. The returned object will be sent to the
+     * client as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
      */
@@ -28,12 +31,12 @@ public class TestEndpoint {
     public String getIt() {
         return "Test endpoint is working!";
     }
-    
+
     @GET
     @Path("headers")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getHeaders(@Context HttpServletRequest req, @Context HttpHeaders hh) {
-        MultivaluedMap<String,String> headers = hh.getRequestHeaders();
+        MultivaluedMap<String, String> headers = hh.getRequestHeaders();
         if (!headers.isEmpty()) {
             Gson gson = new Gson();
             String json = gson.toJson(headers);
@@ -41,5 +44,49 @@ public class TestEndpoint {
         }
         return Response.serverError().build();
     }
-    
+
+    @GET
+    @Path("protected")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProtectedResource(@Context HttpServletRequest req, @Context HttpHeaders hh) {
+
+        // try (final LeakSafeSqlManager mgr = new LeakSafeSqlManager(ucb);
+        // final Authorize auth = Authorize.getIsBasicAuthorized(req)) {
+        // final Authorize auth = Authorize.getIsBasicAuthorized(req);
+
+        // if (!Authorize.getIsBasicAuthorized(req))
+        if (!isAuthorized(req)) {
+            return Response.status(Status.UNAUTHORIZED).entity("Not authorized to access this resource").build();
+        }
+
+        // return a protected resource
+        Resource resource = new Resource();
+        resource.message = "You have access to this protected resource";
+        resource.title = "The Wizard of Oz";
+
+        return Response.status(Status.OK).entity(getJson(resource)).build();
+
+        // }
+        // MultivaluedMap<String,String> headers = hh.getRequestHeaders();
+        // if (!headers.isEmpty()) {
+        // Gson gson = new Gson();
+        // String json = gson.toJson(headers);
+        // return Response.ok(json).build();
+        // }
+        // return Response.serverError().build();
+    }
+
+
+
+    private boolean isAuthorized(HttpServletRequest req) {
+        return Authorize.getIsBasicAuthorized(req);
+    }
+
+    private String getJson(Resource res)
+    {
+        Gson gson = new Gson();
+        String json = gson.toJson(res);
+        return gson.toJson(json, res.getClass());
+    }
+
 }
---
+++ src/main/java/com/bmp/myapi/model/Data.java
@@ -0,0 +1,21 @@
+package com.bmp.myapi.model;
+
+public class Data
+{
+    public static class Message
+    {
+        public String message = "";
+    }
+
+    public static class Resource extends Message
+    {
+        public String title = "";
+    }
+
+    public static class User extends Message
+    {
+
+    }
+
+
+}
